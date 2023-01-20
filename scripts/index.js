f00efdbfdbb848d5436fbbe1f340799608b92688
Sofia Veitko <bon-sofiya@yandex.ru>
2023-01-20 16:21:53 +0300

    refactor: внести замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 // находим форму и ее поля в DOM
-let formElement = document.querySelector('form');
+let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 // данные из профиля
@@ -10,29 +10,29 @@ let editProfileBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closePopupBtn = document.querySelector('.popup__close-btn');
 
-// Открытие Popup
-editProfileBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
+// функция открытия Popup
+function openPopup () {
   popup.classList.add('popup_opened');
   nameInput.value = titleProfile.textContent;
   jobInput.value = subtitleProfile.textContent;
-  }) ;
+}
 
-// Закрытие Popup
-closePopupBtn.addEventListener('click', function(evt) {
-  evt.preventDefault();
+// функция закрытия Popup
+function closePopup () {
   popup.classList.remove('popup_opened');
-  }) ;
+}
 
-
-// Редактирование информации профиля
-// обработчик «отправки» формы
+// Функция редактирования информации профиля
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Отменяем стандартную отправку формы, чтобы определить свою логику отправки.
+    evt.preventDefault(); // отменяем стандартную отправку формы, чтобы определить свою логику отправки.
     titleProfile.textContent = nameInput.value;
     subtitleProfile.textContent = jobInput.value;
-    // закрыть popup
-    popup.classList.remove('popup_opened');
+    closePopup(); // после сохранения закрыть popup
 }
-// прикрепляем обработчик к форме
+
+// слушатель кнопки Close
+closePopupBtn.addEventListener('click', closePopup);
+// слушатель кнопки Edit
+editProfileBtn.addEventListener('click', openPopup);
+// слушатель submit в форме
 formElement.addEventListener('submit', handleFormSubmit);
