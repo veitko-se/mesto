cec8ab7a7ca66614332f0f90989b3dcf114acb54
Sofia Veitko <bon-sofiya@yandex.ru>
2023-01-31 00:23:00 +0300

    feat, refactor: реализовать popup добавления мест (+кнопки like, trash), сделать 6 мест из коробки, упростить понимание кода скрипта js

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,121 @@
-// находим форму и ее поля в DOM
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
-// данные из профиля
-let titleProfile = document.querySelector('.profile__info-title');
-let subtitleProfile = document.querySelector('.profile__info-subtitle');
-// popup и кнопки edit, close
-let editProfileBtn = document.querySelector('.profile__edit-btn');
-let popup = document.querySelector('.popup');
-let closePopupBtn = document.querySelector('.popup__close-btn');
+// раздел Профиль + кнопка Add
+const titleProfile = document.querySelector('.profile__info-title');
+const subtitleProfile = document.querySelector('.profile__info-subtitle');
+const editProfileBtn = document.querySelector('.profile__edit-btn');
+const addPlaceBtn = document.querySelector('.profile__add-btn');
+
+// popup-ы
+const popupProfile = document.querySelector('#popup-profile');
+const formProfile = document.querySelector('#form-profile');
+const inputNameProfile = formProfile.querySelector('#input-profile-name');
+const inputJobProfile = formProfile.querySelector('#input-profile-job');
+
+const closePopupBtn = document.querySelectorAll('.popup__close-btn');
+
+const popupPlace = document.querySelector('#popup-place');
+const formPlace = document.querySelector('#form-place');
+
+//раздел Места
+const containerPlaces = document.querySelector('.elements');
+const initialPlaces = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 
 // функция открытия Popup
-function openPopup () {
+function openPopup(popup) {
   popup.classList.add('popup_opened');
-  nameInput.value = titleProfile.textContent;
-  jobInput.value = subtitleProfile.textContent;
 }
 
 // функция закрытия Popup
-function closePopup () {
+function closePopup(evt) {
+  const popup = evt.target.closest('.popup');
   popup.classList.remove('popup_opened');
 }
 
 // Функция редактирования информации профиля
-function handleFormSubmit (evt) {
-    evt.preventDefault(); // отменяем стандартную отправку формы, чтобы определить свою логику отправки.
-    titleProfile.textContent = nameInput.value;
-    subtitleProfile.textContent = jobInput.value;
-    closePopup(); // после сохранения закрыть popup
+function editProfile(evt) {
+  evt.preventDefault();
+  titleProfile.textContent = inputNameProfile.value;
+  subtitleProfile.textContent = inputJobProfile.value;
+  closePopup(evt);
 }
 
-// слушатель кнопки Close
-closePopupBtn.addEventListener('click', closePopup);
+//функция добавления карточек и кнопок на них
+function addPlace(name, link) {
+  const templatePlace = document.querySelector('#element-template').content;
+  const cardPlace = templatePlace.querySelector('.element').cloneNode(true);
+  const deletePlaceBtn = cardPlace.querySelector('.element__trash-btn');
+  const likePlaceBtn = cardPlace.querySelector('.element__like-btn');
+
+  cardPlace.querySelector('.element__text').textContent = name;
+  cardPlace.querySelector('.element__image').src = link;
+  cardPlace.querySelector('.element__image').alt = name;
+
+  containerPlaces.prepend(cardPlace);
+
+  deletePlaceBtn.addEventListener('click', () => cardPlace.remove());
+  likePlaceBtn.addEventListener('click', evt =>
+    evt.target.classList.toggle('element__like-btn_active')
+  );
+};
+
+// функция генерации карточки из заполненных input по событию
+function renderPlace(evt) {
+  evt.preventDefault();
+  const namePlace = formPlace.querySelector('#input-place-name');
+  const linkPlace = formPlace.querySelector('#input-place-link');
+
+  addPlace(namePlace.value, linkPlace.value);
+  closePopup(evt);
+
+  namePlace.value = '';
+  linkPlace.value = '';
+}
+
+
+//6 карточек из коробки
+initialPlaces.forEach(function (item) {
+  addPlace(item.name, item.link);
+});
+
+
+// слушатель кнопок Close
+closePopupBtn.forEach(function (item) {
+  item.addEventListener('click', evt => closePopup(evt));
+});
 // слушатель кнопки Edit
-editProfileBtn.addEventListener('click', openPopup);
-// слушатель submit в форме
-formElement.addEventListener('submit', handleFormSubmit);
+editProfileBtn.addEventListener('click', () => {
+  openPopup(popupProfile);
+  inputNameProfile.value = titleProfile.textContent;
+  inputJobProfile.value = subtitleProfile.textContent;
+});
+// слушатель кнопки Add
+addPlaceBtn.addEventListener('click', () => openPopup(popupPlace));
+// слушатель submit в форме Profile
+formProfile.addEventListener('submit', evt => editProfile(evt, popupProfile));
+// слушатель submit в форме Place
+formPlace.addEventListener('submit', evt => renderPlace(evt));
